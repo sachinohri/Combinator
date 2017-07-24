218381db1270152ad08ff69baecf2c6f0307be1a
Sachin Ohri <Sachin_Ohri@ad.infosys.com>
2017-07-24 22:21:31 +0530

    Changes for UI

--- src/app/dashboard/news/news.component.ts
+++ src/app/dashboard/news/news.component.ts
@@ -45,7 +45,31 @@ export class NewsComponent implements OnInit {
             urlToImage:"http://static.nfl.com/static/content/public/photo/2017/07/22/0ap3000000821310_thumbnail_200_150.jpg",
             publishedAt:"2017-07-22T20:30:00Z"
             });
-
+          articles.push({
+            author:"Kevin Patra",
+            title:"Antonio Brown to Le'Veon Bell: Steelers 'need' RB",
+            description:"The Steelers' Antonio Brown says he had a \"top secret\" conversation with teammate Le'Veon Bell in which the star wide receiver expressed how important Bell is to Pittsburgh.",
+            url:"http://www.nfl.com/news/story/0ap3000000821334/article/antonio-brown-to-leveon-bell-steelers-need-rb",
+            urlToImage:"http://static.nfl.com/static/content/public/photo/2017/07/17/0ap3000000820197_thumbnail_200_150.jpg",
+            publishedAt:"2017-07-24T11:43:48Z"
+            });
+          articles.push({
+            author:"Edward Lewis",
+            title:"Jabrill Peppers inks rookie deal with Cleveland Browns",
+            description:"The Cleveland Browns have their 2017 draft class all wrapped up. The team announced Jabrill Peppers signed his rookie deal Sunday, making him the last Browns pick to sign.",
+            url:"http://www.nfl.com/news/story/0ap3000000821333/article/jabrill-peppers-inks-rookie-deal-with-cleveland-browns",
+            urlToImage:"http://static.nfl.com/static/content/public/photo/2017/07/23/0ap3000000821329_thumbnail_200_150.jpg",
+            publishedAt:"2017-07-24T01:22:00Z"
+            });
+          articles.push({
+            author:"Edward Lewis",
+            title:"Jerry Jones: Jaylon Smith's knee 'looks really good'",
+            description:"The Cowboys are going to be cautious with Jaylon Smith this summer, but per Jerry Jones, everything with the linebacker's knee \"looks really good.\" What else did we learn at Cowboys camp Sunday?",
+            url:"http://www.nfl.com/news/story/0ap3000000821327/article/jerry-jones-jaylon-smiths-knee-looks-really-good",
+            urlToImage:"http://static.nfl.com/static/content/public/photo/2017/07/23/0ap3000000821326_thumbnail_200_150.jpg",
+            publishedAt:"2017-07-23T23:49:00Z"
+            });
+          
     return articles;
   }
 
